b219049aee96e99e14c8e78befcdc53bc9d70f43
David Verbenyi <david.verbenyi@airapps.com>
2021-05-30 14:05:01 +0100

    Remove Trailing Whitespace from ColorSupport.swift

--- MVP-C-Onboarding/Helpers/ColorSupport.swift
+++ MVP-C-Onboarding/Helpers/ColorSupport.swift
@@ -16,112 +16,112 @@ public enum ColorSupport {
         }
         return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
     }
-  
+
     public static var secondaryLabel: UIColor {
         if #available(iOS 13, *) {
             return .secondaryLabel
         }
         return UIColor(red: 60.0 / 255.0, green: 60.0 / 255.0, blue: 67.0 / 255.0, alpha: 0.6)
     }
-  
+
     public static var tertiaryLabel: UIColor {
         if #available(iOS 13, *) {
             return .tertiaryLabel
         }
         return UIColor(red: 60.0 / 255.0, green: 60.0 / 255.0, blue: 67.0 / 255.0, alpha: 0.3)
     }
-  
+
     public static var quaternaryLabel: UIColor {
         if #available(iOS 13, *) {
             return .quaternaryLabel
         }
         return UIColor(red: 60.0 / 255.0, green: 60.0 / 255.0, blue: 67.0 / 255.0, alpha: 0.18)
     }
-  
+
     public static var systemFill: UIColor {
         if #available(iOS 13, *) {
             return .systemFill
         }
         return UIColor(red: 120.0 / 255.0, green: 120.0 / 255.0, blue: 128.0 / 255.0, alpha: 0.2)
     }
-  
+
     public static var secondarySystemFill: UIColor {
         if #available(iOS 13, *) {
             return .secondarySystemFill
         }
         return UIColor(red: 120.0 / 255.0, green: 120.0 / 255.0, blue: 128.0 / 255.0, alpha: 0.16)
     }
-  
+
     public static var tertiarySystemFill: UIColor {
         if #available(iOS 13, *) {
             return .tertiarySystemFill
         }
         return UIColor(red: 118.0 / 255.0, green: 118.0 / 255.0, blue: 128.0 / 255.0, alpha: 0.12)
     }
-  
+
     public static var quaternarySystemFill: UIColor {
         if #available(iOS 13, *) {
             return .quaternarySystemFill
         }
         return UIColor(red: 116.0 / 255.0, green: 116.0 / 255.0, blue: 128.0 / 255.0, alpha: 0.08)
     }
-  
+
     public static var placeholderText: UIColor {
         if #available(iOS 13, *) {
             return .placeholderText
         }
         return UIColor(red: 60.0 / 255.0, green: 60.0 / 255.0, blue: 67.0 / 255.0, alpha: 0.3)
     }
-  
+
     public static var systemBackground: UIColor {
         if #available(iOS 13, *) {
             return .systemBackground
         }
         return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
-  
+
     public static var secondarySystemBackground: UIColor {
         if #available(iOS 13, *) {
             return .secondarySystemBackground
         }
         return UIColor(red: 242.0 / 255.0, green: 242.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var tertiarySystemBackground: UIColor {
         if #available(iOS 13, *) {
             return .tertiarySystemBackground
         }
         return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
-  
+
     public static var systemGroupedBackground: UIColor {
         if #available(iOS 13, *) {
             return .systemGroupedBackground
         }
         return UIColor(red: 242.0 / 255.0, green: 242.0 / 255, blue: 247.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var secondarySystemGroupedBackground: UIColor {
         if #available(iOS 13, *) {
             return .secondarySystemGroupedBackground
         }
         return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
-  
+
     public static var tertiarySystemGroupedBackground: UIColor {
         if #available(iOS 13, *) {
             return .tertiarySystemGroupedBackground
         }
         return UIColor(red: 242.0 / 255.0, green: 242.0 / 255, blue: 247.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var separator: UIColor {
         if #available(iOS 13, *) {
             return .separator
         }
         return UIColor(red: 60.0 / 255.0, green: 60.0 / 255.0, blue: 67.0 / 255.0, alpha: 0.29)
     }
-  
+
     public static var opaqueSeparator: UIColor {
         if #available(iOS 13, *) {
             return .opaqueSeparator
@@ -226,28 +226,28 @@ public enum ColorSupport {
         }
         return UIColor(red: 174.0 / 255.0, green: 174.0 / 255.0, blue: 178.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var systemGray3: UIColor {
         if #available(iOS 13, *) {
             return .systemGray3
         }
         return UIColor(red: 199.0 / 255.0, green: 199.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var systemGray4: UIColor {
         if #available(iOS 13, *) {
             return .systemGray4
         }
         return UIColor(red: 209.0 / 255.0, green: 209.0 / 255.0, blue: 214.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var systemGray5: UIColor {
         if #available(iOS 13, *) {
             return .systemGray5
         }
         return UIColor(red: 229.0 / 255.0, green: 229.0 / 255.0, blue: 234.0 / 255.0, alpha: 1.0)
     }
-  
+
     public static var systemGray6: UIColor {
         if #available(iOS 13, *) {
             return .systemGray6
